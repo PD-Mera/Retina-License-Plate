0aa5cb3c93b623e8a7cc97e6b71f856603b818ef
PD-Mera <phuongdong1772000@gmail.com>
2022-12-27 11:43:10 +0700

    Fix mobilenetv3

--- data/config.py
+++ data/config.py
@@ -22,18 +22,18 @@ cfg_mnet = {
 
 cfg_re50 = {
     'name': 'Resnet50',
-    'min_sizes': [[16, 32], [64, 128], [256, 512]],
+    'min_sizes': [[16, 32], [48, 96], [160, 224]],
     'steps': [8, 16, 32],
     'variance': [0.1, 0.2],
     'clip': False,
     'loc_weight': 2.0,
     'gpu_train': True,
     'batch_size': 24,
-    'ngpu': 4,
+    'ngpu': 1,
     'epoch': 100,
     'decay1': 70,
     'decay2': 90,
-    'image_size': 840,
+    'image_size': 224,
     'pretrain': True,
     'return_layers': {'layer2': 1, 'layer3': 2, 'layer4': 3},
     'in_channel': 256,
@@ -42,6 +42,27 @@ cfg_re50 = {
 
 cfg_re18 = {
     'name': 'Resnet18',
+    'min_sizes': [[16, 32], [48, 96], [160, 224]],
+    'steps': [8, 16, 32],
+    'variance': [0.1, 0.2],
+    'clip': False,
+    'loc_weight': 2.0,
+    'gpu_train': True,
+    'batch_size': 24,
+    'ngpu': 1,
+    'epoch': 80,
+    'decay1': 70,
+    'decay2': 90,
+    'image_size': 224,
+    'pretrain': True,
+    'return_layers': {'layer2': 1, 'layer3': 2, 'layer4': 3},
+    'in_channel': 64,
+    'out_channel': 256
+}
+
+
+cfg_mobilenetv3 = {
+    'name': 'MobilenetV3',
     'min_sizes': [[16, 32], [48, 96], [160, 224]],
     'steps': [8, 16, 32],
     'variance': [0.1, 0.2],
